f83f7401c4e27728233b14984934deffdc7edf15
GeorgeGUS <georgegus1@gmail.com>
2016-02-16 04:07:15 +0300

    Создал новый файл скриптов, исправил поведение формы обратной связи при незаполненных полях.

---
+++ js/script.js
@@ -0,0 +1,83 @@
+var mapLink = document.querySelector(".map-link");
+var mapWindow = document.querySelector(".map-image-big");
+
+var feedbackBtn = document.querySelector(".feedback-btn");
+var feedbackWindow = document.querySelector(".feedback-form");
+var form = feedbackWindow.querySelector("form");
+var nameCust = feedbackWindow.querySelector("[name=customer-name]");
+var emailCust = feedbackWindow.querySelector("[name=customer-email]");
+var messageCust = feedbackWindow.querySelector("[name=customer-message]");
+
+var storageNameCust = localStorage.getItem("nameCust");
+var storageEmailCust = localStorage.getItem("emailCust");
+
+var buyBtn = document.querySelectorAll(".buy");
+var cartWindow = document.querySelector(".cart-form");
+var closeCart = document.querySelector(".cart-btn.white");
+
+var popup = document.querySelectorAll(".modal-window");
+var closeBtn = document.querySelectorAll(".close-btn");
+
+mapLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  mapWindow.classList.add("modal-active");
+});
+
+feedbackBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  feedbackWindow.classList.add("modal-active");
+  if (storageNameCust && storageEmailCust) {
+    nameCust.value = storageNameCust;
+    emailCust.value = storageEmailCust;
+    messageCust.focus();
+  } else {
+    nameCust.focus();
+  }
+});
+
+for (var n = 0; n < buyBtn.length; n++) {
+  buyBtn[n].addEventListener("click", function (event) {
+    event.preventDefault();
+    cartWindow.classList.add("modal-active");
+  });
+}
+
+closeCart.addEventListener("click", function (event) {
+  event.preventDefault();
+  for (var j = 0; j < popup.length; j++) {
+    popup[j].classList.remove("modal-active");    
+  }
+});
+
+for (var i = 0; i < popup.length; i++) {
+  closeBtn[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    for (var j = 0; j < popup.length; j++) {
+      popup[j].classList.remove("modal-active");
+      feedbackWindow.classList.remove("modal-error");
+    }
+  });
+}
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    for (var j = 0; j < popup.length; j++) {
+      if (popup[j].classList.contains("modal-active")) {
+        popup[j].classList.remove("modal-active");
+        feedbackWindow.classList.remove("modal-error");
+      }
+    }
+  }
+});
+
+form.addEventListener("submit", function (event) {
+  if (!nameCust.value || !emailCust.value || !messageCust.value) {
+    event.preventDefault();
+    feedbackWindow.classList.remove("modal-error");
+    feedbackWindow.offsetWidth = feedbackWindow.offsetWidth;
+    feedbackWindow.classList.add("modal-error");
+  } else {
+    localStorage.setItem("nameCust", nameCust.value);
+    localStorage.setItem("emailCust", emailCust.value);
+  }
+});
