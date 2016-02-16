53bbb52ef8c72c88fa25a4e100e4f00530ea6049
GeorgeGUS <georgegus1@gmail.com>
2016-02-17 01:35:37 +0300

    Добавил проверку на наличие значений в переменных.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 var mapLink = document.querySelector(".map-link");
 var mapWindow = document.querySelector(".map-image-big");
 
+var popup = document.querySelectorAll(".modal-window");
+var closeBtn = document.querySelectorAll(".close-btn");
+
 var feedbackBtn = document.querySelector(".feedback-btn");
 var feedbackWindow = document.querySelector(".feedback-form");
-var form = feedbackWindow.querySelector("form");
-var nameCust = feedbackWindow.querySelector("[name=customer-name]");
-var emailCust = feedbackWindow.querySelector("[name=customer-email]");
-var messageCust = feedbackWindow.querySelector("[name=customer-message]");
+
+if (feedbackWindow) {
+  var form = feedbackWindow.querySelector("form");
+  var nameCust = feedbackWindow.querySelector("[name=customer-name]");
+  var emailCust = feedbackWindow.querySelector("[name=customer-email]");
+  var messageCust = feedbackWindow.querySelector("[name=customer-message]");
+}
 
 var storageNameCust = localStorage.getItem("nameCust");
 var storageEmailCust = localStorage.getItem("emailCust");
@@ -15,25 +21,26 @@ var buyBtn = document.querySelectorAll(".buy");
 var cartWindow = document.querySelector(".cart-form");
 var closeCart = document.querySelector(".cart-btn.white");
 
-var popup = document.querySelectorAll(".modal-window");
-var closeBtn = document.querySelectorAll(".close-btn");
+if (mapLink) {
+  mapLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    mapWindow.classList.add("modal-active");
+  });
+}
 
-mapLink.addEventListener("click", function (event) {
-  event.preventDefault();
-  mapWindow.classList.add("modal-active");
-});
-
-feedbackBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  feedbackWindow.classList.add("modal-active");
-  if (storageNameCust && storageEmailCust) {
-    nameCust.value = storageNameCust;
-    emailCust.value = storageEmailCust;
-    messageCust.focus();
-  } else {
-    nameCust.focus();
-  }
-});
+if (feedbackBtn) {
+  feedbackBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    feedbackWindow.classList.add("modal-active");
+    if (storageNameCust && storageEmailCust) {
+      nameCust.value = storageNameCust;
+      emailCust.value = storageEmailCust;
+      messageCust.focus();
+    } else {
+      nameCust.focus();
+    }
+  });
+}
 
 for (var n = 0; n < buyBtn.length; n++) {
   buyBtn[n].addEventListener("click", function (event) {
@@ -70,14 +77,16 @@ window.addEventListener("keydown", function (event) {
   }
 });
 
-form.addEventListener("submit", function (event) {
-  if (!nameCust.value || !emailCust.value || !messageCust.value) {
-    event.preventDefault();
-    feedbackWindow.classList.remove("modal-error");
-    feedbackWindow.offsetWidth = feedbackWindow.offsetWidth;
-    feedbackWindow.classList.add("modal-error");
-  } else {
-    localStorage.setItem("nameCust", nameCust.value);
-    localStorage.setItem("emailCust", emailCust.value);
-  }
-});
+if (form) {
+  form.addEventListener("submit", function (event) {
+    if (!nameCust.value || !emailCust.value || !messageCust.value) {
+      event.preventDefault();
+      feedbackWindow.classList.remove("modal-error");
+      feedbackWindow.offsetWidth = feedbackWindow.offsetWidth;
+      feedbackWindow.classList.add("modal-error");
+    } else {
+      localStorage.setItem("nameCust", nameCust.value);
+      localStorage.setItem("emailCust", emailCust.value);
+    }
+  });
+}
